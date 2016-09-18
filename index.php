b68124d084064d168229a53724ba47d18a2aab0f
Alex <alex@homehero.org>
2016-09-17 21:51:57 -0700

    added css folder

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Falta-Uno</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+  </head>
+  <body>
+    <div class="header">
+      <h1 class="logo"><a href="/"</a>Falta Uno</h1>
+
+    </div>
+
+  </body>
+</html>
